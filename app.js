76eb854bdcd261db6078eb04e95487de7f522e68
herald-noel <bontilao.heraldnoel@gmail.com>
2023-08-29 16:26:12 +0800

    Refactor and Improve javascript codebase
    The refactoring results in cleaner code.
    
    Original Contributor: @knee-son

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
-const textarea = document.getElementById("user-text");
+// Selectors
+const userText = document.getElementById("user-text");
 
-textarea.placeholder =
-`Usage:
-1. Enter the text you want to transform into the textarea.
-2. Click on one of the transformation buttons:
-  - Lower Case: Transforms the text to all lower case.
-  - UPPER CASE: Transforms the text to all upper case.
-  - Title Case: Transforms the text to Title Case, where the first letter of each word is capitalized.
-3. The transformed text will be displayed in the textarea.`;
+const lowerCaseBtn = document.getElementById("lower-btn");
+const upperCaseBtn = document.getElementById("upper-btn");
+const titleCaseBtn = document.getElementById("title-btn");
 
-document.getElementById("lc").addEventListener("click", () => {
-  const text = textarea.value;
-  textarea.value = text.toLowerCase();
+// Event Listeners
+lowerCaseBtn.addEventListener("click", () => {
+  const text = userText.value;
+  userText.value = text.toLowerCase();
 });
 
-document.getElementById("uc").addEventListener("click", () => {
-  const text = textarea.value;
-  textarea.value = text.toUpperCase();
+upperCaseBtn.addEventListener("click", () => {
+  const text = userText.value;
+  userText.value = text.toUpperCase();
 });
 
-document.getElementById("tc").addEventListener("click", () => {
-  const text = textarea.value;
-  
-  textarea.value = text
-    //knee son text tools
+titleCaseBtn.addEventListener("click", () => {
+  const text = userText.value;
+
+  userText.value = text
     .split(" ")
-    //['knee','son', 'text', 'tools']
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    //['K'+'nee','S'+'on', 'T'+'ext', 'T'+'ools']
     .join(" ");
-    //'Knee'+' '+'Son'+''+'Text'+''+'Tools'
 });
